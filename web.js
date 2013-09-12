a7ec60201b324b7883f5985de3eca16046d56afc
Ubuntu <ubuntu@ip-172-31-10-49.us-west-2.compute.internal>
2013-09-12 16:32:19 +0000

    modified:   web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello Leukippos team, lets code together! This code should be found at https://github.com/jarlemag/Heroku');
 });
 
 var port = process.env.PORT || 5000;
